18ce05c6fa9459defe7170e75d82327c0fb6d04e
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-05-17 22:10:26 -0700

    comment form done

---
+++ app/forms/comment_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import IntegerField, StringField, SubmitField, DecimalField
+from wtforms.validators import DataRequired, NumberRange, Length
+
+
+class CommentForm(FlaskForm):
+    comment_body = StringField('Comment', validators=[DataRequired(), Length(min=5, max=500, message='Comment must be between 5 and 500 characters')])
