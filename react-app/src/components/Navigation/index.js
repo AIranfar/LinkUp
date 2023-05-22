ad0b08c3fcca78ee9c60e588d6a2e71cdfec9fc3
Ashkaun Iranfar <ashkauniranfar@gmail.com>
2023-05-22 13:53:41 -0700

    fixed nav bar

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import LoginFormModal from '../LoginFormModal';
+import OpenModalButton from '../OpenModalButton';
+import SignupFormModal from '../SignupFormModal';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
-function Navigation({ isLoaded }){
+function Navigation({ isLoaded }) {
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
 		<ul>
 			<ul>
-				<NavLink exact to="/posts">LinkUp</NavLink>
+				<NavLink exact to="/">Home</NavLink>
 			</ul>
 			{isLoaded && (
-				<ul>
-					<ProfileButton user={sessionUser} />
-				</ul>
+				<div>
+					{sessionUser ? (
+						<ul>
+							<ProfileButton user={sessionUser} />
+						</ul>
+					) : (
+						<>
+							<OpenModalButton
+								buttonText="Log In"
+								modalComponent={<LoginFormModal />}
+							/>
+
+							<OpenModalButton
+								buttonText="Sign Up"
+								modalComponent={<SignupFormModal />}
+							/>
+						</>
+					)}
+				</div>
 			)}
 		</ul>
 	);
